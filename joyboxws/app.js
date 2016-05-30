f42a3de4ee6cb06aa34eee911eb1b7f0adf9ac27
nghia nguyen <nghia.nguyen@zeisicmienbac.com>
2016-05-30 15:46:56 +0700

    update joybox web service

--- joyboxws/app.js
+++ joyboxws/app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 
 
+
+app.use(express.body-parser());
 app.use(bodyParser.json());
 app.listen(7000);
 
@@ -10,9 +12,10 @@ app.post('/joyboxws/',  function(req, res){
 	
 	console.log("JOYBOX WEB SERVICE");
 	
-	var jsonPost = JSON.parse(req.boy);
-	console.log("request. body: ", jsonPost.body);
-	console.log("action: ", jsonPost.body.result.action);
+	var jsonPost = JSON.parse(req.body);
+	console.log("request. body: ", req.body);
+	
+	console.log("action: ", jsonPost.result.action);
 	
 	
 });
