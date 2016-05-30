11f337f97ddb001b3c1cee3b4fa3b895c08d625c
nghia nguyen <nghia.nguyen@zeisicmienbac.com>
2016-05-30 16:03:11 +0700

    update joybox web service

--- joyboxws/app.js
+++ joyboxws/app.js
@@ -9,6 +9,8 @@ app.post('/joyboxws/',  function(req, res){
 	
 	console.log("JOYBOX WEB SERVICE");
 	
+	console.log("body: ",req.body);
+	
 	var jsonPost = JSON.parse(req.body);
 	console.log("request. body: ", jsonPost.body);	
 	console.log("action: ", jsonPost.result.action);
